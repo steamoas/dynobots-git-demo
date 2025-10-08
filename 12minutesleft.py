92f68bffea5656340597fd8eea4e268e80c57c11
steam <steam@ozarkschool.org>
2025-10-08 15:23:35 -0500

    10/8

---
+++ 12minutesleft.py
@@ -0,0 +1,4 @@
+from robot import drive, left_attach, right_attach
+left_attach.run_time(300, 15000, wait=False)
+right_attach.run_time(300, 15000, wait=False)
+drive.turn(2880)
--- robot.py
+++ robot.py
@@ -7,10 +7,10 @@ hub = PrimeHub()
 #Adjust these parameters to fit your robot. Also ensure that you add any sensors here.
 
 left_drive = Motor(Port.D, Direction.COUNTERCLOCKWISE)
-right_drive = Motor(Port.F, Direction.CLOCKWISE)
+right_drive = Motor(Port.C, Direction.CLOCKWISE)
 
 left_attach = Motor(Port.A, Direction.CLOCKWISE)
-right_attach = Motor(Port.E, Direction.CLOCKWISE)
+right_attach = Motor(Port.B, Direction.CLOCKWISE)
 
-drive = DriveBase(left_drive, right_drive, 60, 104)
+drive = DriveBase(left_drive, right_drive, 62.4, 118)
 drive.use_gyro(True)
