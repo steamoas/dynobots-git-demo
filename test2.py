601cf90ec551200811995a4ee59eb73f34133738
steam <steam@ozarkschool.org>
2025-11-03 13:18:05 -0800

    11/3 pre-practice

--- test2.py
+++ test2.py
@@ -1,4 +1,14 @@
-from robot import drive,right_attach
-drive.straight(350)
-drive.turn(-98)
-drive.straight(300)
+from robot import drive,left_attach
+
+#left_attach.run_until_stalled(-100, duty_limit=50);
+drive.curve(62.4,95)
+
+
+
+
+drive.straight(274)
+drive.straight(-200)
+drive.turn(-90)
+drive.straight(150)
+drive.turn(90)
+drive.straight(400)
