7cea3a72bb71d55a87cfff9f6f7569d5f87b71e4
steam <steam@ozarkschool.org>
2025-11-14 10:04:47 -0800

    11/14

--- run2.py
+++ run2.py
@@ -2,14 +2,14 @@ from pybricks.tools import run_task
 from robot import drive,left_attach
 async def run2():
 #left_attach.run_until_stalled(-100, duty_limit=50);
-    await drive.straight(15)
+    await drive.straight(10)
     await drive.curve(62.4,95)
-    await drive.straight(210)
+    await drive.straight(340)
     await drive.straight(-150)
     await drive.turn(-90)
-    await drive.straight(150)
+    await drive.straight(155)
     await drive.turn(90)
-    await drive.straight(400)
+    await drive.straight(250)
 
 if __name__ == "__main__":
     run_task(run2())
