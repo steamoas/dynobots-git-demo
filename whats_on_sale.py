a3633a5150793fe5b8b607a5feeda6388ad37779
steam <steam@ozarkschool.org>
2026-02-25 16:53:17 -0600

    Rodney

--- whats_on_sale.py
+++ whats_on_sale.py
@@ -8,12 +8,15 @@ from robot import drive, right_attach
 
 async def whats_on_sale():
     await drive.straight(250)
-    await drive.turn(-54)
-    await drive.straight(200)
+    await drive.turn(-52)
+    await drive.straight(198)
     await right_attach.run_angle(100, 110)
     await drive.straight(-95)
-    await right_attach.run_angle(100, -100)
-    await drive.straight(-200)
+    await right_attach.run_angle(100, -85)
+    await drive.straight(-30)
+    await right_attach.run_angle(-100, -58)
+    await drive.turn(-180)
+    await drive.straight(130)
 
 
 if __name__ == "__main__":
