ec74658d93e3c72ebe84e5748ec2649ddeba7531
steam <steam@ozarkschool.org>
2026-03-17 16:54:52 -0500

    LILY

--- heavy_lifting.py
+++ heavy_lifting.py
@@ -7,9 +7,16 @@ from pybricks.tools import run_task, wait
 import careful_recovery
 from robot import drive, left_attach
 
+
 async def heavy_lifting():
+    await drive.straight(100)
+    await drive.turn(45)
+    await drive.straight(250)
+    await drive.turn(-40)
+    await drive.straight(400)
+    await drive.turn(-20)
+    await drive.turn()
 
-    await drive.curve(150,100)
 
-if __name__== "__main__":
+if __name__ == "__main__":
     run_task(heavy_lifting())
