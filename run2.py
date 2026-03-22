2066f3f90b40f7886b7911ac19dfe7d2f5f7b34b
steam <steam@ozarkschool.org>
2026-03-22 15:52:39 -0500

    LILY and MIA

--- run2.py
+++ run2.py
@@ -1,15 +1,23 @@
-from pybricks.tools import run_task
-from robot import drive,left_attach
+from pybricks.tools import run_task, wait
+from robot import drive, left_attach
+
+
 async def run2():
-#left_attach.run_until_stalled(-100, duty_limit=50);
+    # left_attach.run_until_stalled(-100, duty_limit=50);
     await drive.straight(50)
-    await drive.curve(62.4,95)
-    await drive.straight(400)
+    await drive.curve(50, 100)
+
+    # await drive.straight(390)
+    drive.drive(300, 0)
+    await wait(1450)
     await drive.straight(-150)
     await drive.turn(-90)
-    await drive.straight(20)
+    await drive.straight(200)
     await drive.turn(90)
     await drive.straight(250)
+    drive.settings(straight_speed=400)
+    await drive.straight(-850)
+
 
 if __name__ == "__main__":
     run_task(run2())
