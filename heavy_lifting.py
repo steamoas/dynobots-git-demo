c811aa53a482944f4c01490c943033a995652d62
steam <steam@ozarkschool.org>
2026-03-23 16:10:41 -0500

    Victoria

--- heavy_lifting.py
+++ heavy_lifting.py
@@ -9,19 +9,19 @@ from robot import drive, left_attach
 
 
 async def heavy_lifting():
-
-    await left_attach.run_angle(100, -45)
+    await left_attach.run_until_stalled(300, duty_limit=25)
+    await left_attach.run_angle(100, -50)
     await drive.straight(100)
     await drive.turn(45)
     await drive.straight(225)
     await drive.turn(-40)
     await drive.straight(400)
     await drive.turn(-22)
-    # await left_attach.run_angle(300, -35)
+    await left_attach.run_angle(300, -35)
     await drive.straight(160)
     await left_attach.run_angle(300, -100)
     await drive.turn(40)
-    await drive.straight(145)
+    await drive.straight(140)
 
 
 if __name__ == "__main__":
