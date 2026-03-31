5d6df30397a2003b300af09af1d69af8436e7ab9
steam <steam@ozarkschool.org>
2026-03-31 16:55:10 -0500

    Victoria

--- whats_on_sale.py
+++ whats_on_sale.py
@@ -9,7 +9,7 @@ from robot import drive, right_attach
 async def whats_on_sale():
     await drive.straight(250)
     await drive.turn(-52)
-    await drive.straight(210)
+    await drive.straight(200)
     await right_attach.run_angle(100, 110)
     await drive.straight(-95)
     await right_attach.run_angle(100, -85)
